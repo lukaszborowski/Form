9ea45d0c0e872192648738c7dbd85b48eb160caf
lukaszborowski <lukasz1borowski@gmail.com>
2020-10-25 13:59:23 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-    // Zmienne
+    // Variables
 
     const valName = $("#validationName");
     const valSurr = $("#validationSurr");
@@ -34,145 +34,236 @@ document.addEventListener("DOMContentLoaded", function() {
                     "Wielkopolskie",
                     "Zachodniopomorskie"
                     ];
-
-    // insert State
-    //valState.ready(()=> {
-//
-    //    for(let i=1; i<=states.length; i++){
-    //       // let number = i +1;
-    //        //console.log(number);
-    //        $("#validationState").append('<option>' + states[i] + '</option>');
-//
-    //    }
-
-        // for (let el of states){
-        //     $("#validationState").append('<option>' + el + '</option>');
-//
-//
-        // }
-
-    //});
+    const datePick = new Date();
+    const year = datePick.getFullYear();
+    const month = datePick.getMonth();
+    const callbacks = $.Callbacks();
+    const regName = /[A-Z][a-zA-Z][^#&<>\"@~;$^%{}?!*|`_+=-]{1,20}$/i;
+    const regSurr = /[A-Z][a-zA-Z][^#&<>\"@~;$^%{}?!*|`_+=-]{1,20}$/i;
+    const regUser =/[A-Z][a-zA-Z][^#"@~;]{1,20}$/i;
+    const regZip = /^([0-9]{2})(-[0-9]{3})?$/i;
+    const regMail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    const regPhone = /^(\+48\s*)?\d{2}\s*\d{3}(\s*|\-)\d{2}(\s*|\-)\d{2}$/i;
 
 
-    const insertState = function() {
+
+
+    // Functions
+
+    const insertState = ()=> {
         for (let el of states){
             let smlEl = el.toLowerCase();
-            // console.log(smlEl);
-                $("#validationState").append('<option value="">' + el + '</option>');
-                $(" #validationState > option").attr("value", smlEl);
+            const $option = $("<option>", {value: smlEl}).text(el);
+            $("#validationState").append($option);
 
     }};
 
-    const stateValue = function () {
+    const grabClass = (element) => {
+        let parent = element.parents(element);
+        let parentClass = parent.attr("class");
+        // console.log(parentClass);
+        let parentClassFix = parentClass.slice(14,24);
+        const selector = $("pa")
+        // let parentClassFixed = "."+parentClassFix;
+        // const correct = document.getElementsByClassName('parentClassFixed');
 
-        // for(let i=1; i<=states.length; i++){
-        //            let number = i +1;
-        //             console.log(number);
-        //             $("#validationState:nth-child(i)").append('<option>' + states[i] + '</option>');
+        // console.log(correct);
+            // $('parentClassFixed > .validation');
+        // return (correct);
+        // console.log(parentClass);
+        // console.log(parentClassFix);
+
+    };
+
+    // Permisions
+    const noPerm = (element)=> {
+        correct().removeClass("isValid", "restricted-perm").addClass("isInvalid").text('Wrong - too young');
+        lookBad(element);
+    };
+
+    const restriPerm = ()=> {
+        $(".age-line > .validation").removeClass("isValid", "isInvalid").addClass("restricted-perm").text('Restricted Access');
+    };
+
+    const fullPerm = ()=> {
+        $(".age-line > .validation").removeClass("isInvalid", "restricted-perm").addClass("isValid").text("Looks Good");
+        lookGood(element)
+    };
+
+    const insertAge = (first, second, element)=> {
+
+        const teenager = Number(13);
+        const adult = Number(18);
+        valAge.val(first);
+
+        if(first < teenager) {
+            noPerm();
+        }else if(first === teenager && second < month){
+            noPerm();
+        }else if(first === teenager && second >= month){
+            restriPerm();
+        }else if(first > teenager && first < adult){
+            restriPerm()
+        }else if(first === adult && second < month) {
+            restriPerm();
+        }else if(first === adult && second >= month){
+            fullPerm();
+        }else if(first > adult){
+            fullPerm();
+        }
+
+    };
 
 
-     };
+    // const Validation = function(){
+    //     this.table = [];
+    //     this.validated = 0;
+    //     this.unvalidated = 0;
+    // };
+    //
+    // Validation.prototype.addValid = function(name, valid){
+    //     const object = {
+    //         name: name,
+    //         valid: valid,
+    //     };
+    //     this.table.push(object);
+    //
+    // };
+    //
+    // Validation.prototype.addInvalid = function(name, invalid){
+    //     const object = {
+    //         name: name,
+    //         valid: invalid,
+    //     };
+    //     this.table.push(object);
+    // };
 
-    const callbacks = $.Callbacks();
+    const lookGood = (element) => {
+
+        element.css("background-color", "rgba(79, 248, 32, 0.20)");
+        element.addClass("was-validated");
+
+    };
+
+    const lookBad = (element) => {
+        element.css("background-color", "rgba(250, 0, 0, 0.20)");
+        element.addClass("needs-validation");
+
+    };
+
+    const validation = (element, reg) => {
+        element.on('change', ()=>{
+            let val = element.val();
+
+            if(reg.test(val)){
+                fullPerm();
+            }else {
+                noPerm()
+            }
+        })
+    };
+
     callbacks.add(insertState);
     callbacks.fire();
     callbacks.remove(insertState);
-    callbacks.add(stateValue);
+    callbacks.add(grabClass(valName));
     callbacks.fire();
 
 
-
-
-
-    // Validations
+    //
+    // VALIDATIONS
+    //
 
     // Name Validation
 
-    valName.on('input', () => {
+    valName.on('change', () => {
         let val = valName.val();
         const regName = /[A-Z][a-zA-Z][^#&<>\"@~;$^%{}?!*|`_+=-]{1,20}$/i;
 
         if (regName.test(val)) {
             $(".first-name > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
+            lookGood(valName);
 
 
         } else {
             $(".first-name > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect Name!');
-
+            lookBad(valName)
         }
     });
 
     // Surrname Validation
 
-    valSurr.on('input', () => {
+    valSurr.on('change', () => {
         let val = valSurr.val();
         const regSurr = /[A-Z][a-zA-Z][^#&<>\"@~;$^%{}?!*|`_+=-]{1,20}$/i;
 
         if (regSurr.test(val)) {
             $(".last-name > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-
+            lookGood(valSurr);
         } else {
             $(".last-name > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect Surrname');
-
+            lookBad(valSurr)
         }
     });
 
     // Username Validation
 
-    valUser.on('input', () => {
+    valUser.on('change', () => {
         let val = valUser.val();
         const regUser =/[A-Z][a-zA-Z][^#"@~;]{1,20}$/i;
 
         if (regUser.test(val)) {
             $(".user-name > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-
+            lookGood(valUser);
         } else {
             $(".user-name > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect Username');
-
+            lookBad(valUser)
         }
     });
 
     // Adress Validation
 
-    valAdr.on('input', () => {
+    valAdr.on('change', () => {
         let val = valAdr.val();
 
         if (val.length >= 3) {
             $(".adress-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-
+            lookGood(valAdr);
         } else {
             $(".adress-line > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect Adress');
-
+            lookBad(valAdr)
         }
     });
 
     // City Validation
 
-    valCity.on('input', () => {
+    valCity.on('change', () => {
         let val = valCity.val();
 
         if (val.length >= 3) {
             $(".city-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-
+            lookGood(valCity);
         } else {
             $(".city-line > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect City name');
-
+            lookBad(valCity)
         }
     });
 
     // State Validation
 
-    valState.on('input', () => {
+    valState.on('change', () => {
         let val = valState.val();
 
 
         for(let el of states){
-            if(val === el) {
+           let elem = el.toLowerCase();
+
+            if(val === elem) {
                 $(".state-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-                break;
+                lookGood(valState);
             } else {
                 $(".state-line > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect State');
-
+                lookBad(valState);
             }
         }
 
@@ -180,13 +271,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Zip Validation
 
-    valZip.on('input', () => {
+    valZip.on('change', () => {
         let val = valZip.val();
         const regZip = /^([0-9]{2})(-[0-9]{3})?$/i;
 
         if (regZip.test(val)) {
             $(".zip-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-
+            lookGood(valZip);
         } else {
             $(".zip-line > .validation").removeClass("isValid").addClass("isInvalid").text('Incorrect Post-Code');
 
@@ -195,35 +286,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Mail Validation
 
-    valMail.on('input', () => {
+    valMail.on('change', () => {
         let val = valMail.val();
         const regMail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
         if (!regMail.test(val)) {
 
             $(".mail-line > .validation").removeClass("isValid").addClass("isInvalid").text('Wrong Mail. Try like - sample@sample.com');
+            lookGood(valMail)
         } else {
 
             $(".mail-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
+            lookBad(valMail)
         }
     });
 
     // Phone Validation
 
-    valPhone.on('input', () => {
+    valPhone.on('change', () => {
         let val = valPhone.val();
         const regPhone = /^(\+48\s*)?\d{2}\s*\d{3}(\s*|\-)\d{2}(\s*|\-)\d{2}$/i;
         if (regPhone.test(val)) {
             $(".phone-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
-
+            lookGood(valPhone)
         } else {
             $(".phone-line > .validation").removeClass("isValid").addClass("isInvalid").text('Wrong Phone number');
-
+            lookBad(valPhone)
         }
     });
 
     // Gender Validation
 
-    valGender.on('input', () => {
+    valGender.on('change', () => {
         let val = valGender.val();
 
         if (val === "Male" || val === "Female" || val === "Other" || val === "Dont want to tell") {
@@ -233,14 +326,41 @@ document.addEventListener("DOMContentLoaded", function() {
             $(".gender-line > .validation").removeClass("isValid").addClass("isInvalid").text('Please Choose Gender');
 
         }
-    })
+    });
 
     // M/Y Validation
 
-    valBirth.on('input', () => {
+    valBirth.on('change', () => {
         let val = valBirth.val();
-       // const regBirth =
-    })
+        const regBirth = /^((0[1-9])|(1[0-2]))\/(\d{4})$/i;
+        let inputYear = val.slice(3,7);
+        let inputMonth = val.slice(0,2);
+
+        // To global scope
+        const userYears =  ()=> {
+            return ( Number(year - inputYear));
+
+        };
+
+        const userMonth = ()=> {
+            return (Number(inputMonth))
+        };
+        // end of global scope
+
+        if(regBirth.test(val) && userYears() < Number(125)) {
+            $(".birth-line > .validation").removeClass("isInvalid").addClass("isValid").text("Looks Good");
+            lookGood(valBirth);
+            callbacks.add(insertAge(userYears(), userMonth(),valAge));
+            callbacks.fire();
+            callbacks.remove(insertAge(userYears(), userMonth(), valAge));
+
+        } else {
+            $(".birth-line > .validation").removeClass("isValid").addClass("isInvalid").text('Wrong Date');
+            lookBad(valBirth);
+        }
+
+    });
+
 
 });
 
