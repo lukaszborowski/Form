00883c7132ffd08cfe0a97ec86945b303983f178
lukaszborowski <lukasz1borowski@gmail.com>
2020-11-02 17:21:13 +0100

    pretty done

--- js/app.js
+++ js/app.js
@@ -64,26 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
-    (function () {
-        'use strict';
-        window.addEventListener('load', function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            const forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            const validation = Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
-
-
 
     // Permisions
     const noPerm = (element, validation) => {
@@ -334,11 +314,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            const forms = document.getElementsByClassName('needs-validation');
+            const validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
 
-    // window.on('load', () => {
-    //     const forms = $(".needs-validation");
-    //     console.log(forms);
-    // })
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+
 
 });
 
